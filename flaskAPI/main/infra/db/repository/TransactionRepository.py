1bf59a746a1afaf119dafa37bedb53ab62726bc5
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2023-06-07 14:21:12 -0400

    feat(): APP SETTINGS

--- flaskAPI/main/infra/db/repository/TransactionRepository.py
+++ flaskAPI/main/infra/db/repository/TransactionRepository.py
@@ -8,21 +8,21 @@ from main.domain.transaction.Transaction import Transaction
 from datetime import datetime, timedelta
 import pytz
 
+
 class TransactionRepository:
-    
+
     def __init__(self, transaction_schema: MongoTransactionSchema, connector: MongoConnector):
         self.transaction_schema = transaction_schema
         self.connector = connector
         self.db = self.connector.main.transactions
-    
-    
+
     def add_transaction(self, transaction: Transaction):
         transaction_dict = self.transaction_schema.dump(transaction)
         self.db.insert_one(transaction_dict)
-    
+
     def get_transactions(self, user_id: DomainId) -> List[Transaction]:
         return [self.transaction_schema.load(transaction) for transaction in self.db.find({'user_id': user_id.to_object_id()}).sort('_id', pymongo.DESCENDING)]
-    
+
     def remove_old_transactions(self) -> int:
         now = datetime.now()
         last_month = now - timedelta(days=30)
@@ -33,8 +33,7 @@ class TransactionRepository:
                 '$lte': string_date
             }
         }
-        
+
         results = self.db.delete_many(filter=filter)
-        
+
         return results.deleted_count
-    
