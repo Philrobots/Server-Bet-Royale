e24063dba4abc59a038f0896d97274d74c60cc7d
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2023-06-14 18:25:14 -0400

    feat(): try workers

--- flaskAPI/development.entrypoint.sh
+++ flaskAPI/development.entrypoint.sh
@@ -1,2 +1,2 @@
 #! bin/sh
-gunicorn --threads 5 --reload --bind 0.0.0.0:8000 --chdir /flask run:app
+gunicorn --worker-class=gevent --worker-connections=1000 --workers=3  --reload --timeout 600 --bind 0.0.0.0:8000 --chdir /flask run:app
--- flaskAPI/production.entrypoint.sh
+++ flaskAPI/production.entrypoint.sh
@@ -1,2 +1,2 @@
 #! bin/sh
-gunicorn --workers=2 --threads=5 --worker-class=gthread --limit-request-line 0  --bind 0.0.0.0:8000 --chdir /flask run:app
+gunicorn --worker-class=gevent --worker-connections=1000 --workers=2  --reload --timeout 600 --limit-request-line 0  --bind 0.0.0.0:8000 --chdir /flask run:app
