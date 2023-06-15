438de5e84c5f6730bb03b9793d9cd16e4126803a
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2023-06-15 09:22:55 -0400

    Feat(): threads

--- flaskAPI/development.entrypoint.sh
+++ flaskAPI/development.entrypoint.sh
@@ -1,2 +1,2 @@
 #! bin/sh
-gunicorn --workers=2 --threads=5 --worker-class=gthread --limit-request-line 0  --bind 0.0.0.0:8000 --chdir /flask run:app
+gunicorn -w 1 --threads=12 -worker-class=gthread --limit-request-line 0  --bind 0.0.0.0:8000 --chdir /flask run:app
