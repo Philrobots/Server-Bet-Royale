fb89e7dc82886263cbdcdb4f92592e16d0d5afa2
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2023-06-22 12:36:37 -0400

    API KEY

--- flaskAPI/config.py
+++ flaskAPI/config.py
@@ -3,7 +3,7 @@ class Config(object):
     TESTING = False
     SECRET_KEY = "VitAo;wH7noT2y2c!}yN'?o=X1|:[T]l!RS3;<s_?rW^a-S>+zQ9fItgn>T5{lguuid.uu"
     ADMIN_KEY = "Nq[nn#/uIT$AVYS`{H;:V=,OM[LR8sr!^?B<}u[5jy-]K+^nTLf87Fmil[cMq?k"
-    ODDS_API_KEY = "87c963d43e0c21a33f7d029816a95f77"
+    ODDS_API_KEY = "9j3e4zfnpn4tttmbegcxvcy7gf8fxeqq969vkgwf4sy8tmyecavym6mwgtqmcz93"
 
 
 class ProductionConfig(Config):
